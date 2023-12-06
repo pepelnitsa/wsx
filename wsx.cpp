e5df9564f1e89910fd6441392cf5075c5786172d
pepeln1tsa <ihor.avramchyk@gmail.com>
2023-12-06 20:01:57 +0200

    Добавьте файлы проекта.

---
+++ wsx.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <set>
+using namespace std;
+
+int main() {
+    set<int> tree;
+
+    tree.insert(5);
+    tree.insert(3);
+    tree.insert(8);
+    tree.insert(2);
+    tree.insert(10);
+
+    for (auto elem : tree) {
+        cout << elem << " ";
+    }
+    cout << endl;
+
+    tree.erase(3);
+    tree.erase(8);
+
+    for (auto elem : tree) {
+        cout << elem << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
